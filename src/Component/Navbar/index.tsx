180669202e57fd24a7109929f2b259be2593a1ba
HoangMinhNg <ngminh3004@gmail.com>
2024-06-25 15:17:34 +0700

    update navbar

--- src/Component/Navbar/index.tsx
+++ src/Component/Navbar/index.tsx
@@ -2,7 +2,7 @@ const NavbarAdmin: React.FC = () => {
   return (
     <>
       <div className="relative flex py-2 w-1/3">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+        {/* <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             viewBox="0 0 24 24"
@@ -33,7 +33,7 @@ const NavbarAdmin: React.FC = () => {
           id="input-group-1"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Tìm kiếm"
-        />
+        /> */}
       </div>
       <div className="flex">
         {/* <button
